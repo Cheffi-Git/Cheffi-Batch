5b191b943ede0c1a9e234e6b94758512d182d13e
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-04-24 16:11:43 +0900

    부하 테스트를 위한 쓰레드풀 설정 변경

--- src/main/kotlin/com/message/config/Configurations.kt
+++ src/main/kotlin/com/message/config/Configurations.kt
@@ -1,22 +1,25 @@
 package com.message.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.AsyncConfigurer
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import java.util.concurrent.Executor
 
 @EnableAsync
+@EnableScheduling
 @Configuration
-class AsyncConfig() {
-    @Bean
-    fun eventTaskExecutor(): Executor? {
+class AsyncConfig : AsyncConfigurer {
+
+    override fun getAsyncExecutor(): Executor? {
         val executor = ThreadPoolTaskExecutor()
-        executor.corePoolSize = 4
-        executor.maxPoolSize = 100
-        executor.queueCapacity = 100000
+        executor.corePoolSize = 10
+        executor.maxPoolSize = 15_000
+        executor.queueCapacity = 200_000
         executor.setThreadNamePrefix("Event-")
         executor.initialize()
         return executor
     }
+
 }
